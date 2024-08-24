26d1585ff7b9cc960d97add20b3b653e49154001
roshiroku <shir.oraan@gmail.com>
2024-08-24 19:27:41 +0300

    add model base class with crud functionality

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,15 +1,45 @@
 export default class Model {
+  static api;
   static schema = {};
-
   static labels = {};
+
+  static async load(id) {
+    const data = await this.api.read(id);
+    return new this(data);
+  }
 
   static fromObject({ }) {
     return new Model();
   }
 
-  constructor() { }
+  get api() {
+    return this.constructor.api;
+  }
+
+  constructor({ _id = "", createdAt = "" } = {}) {
+    this._id = _id;
+    this.createdAt = createdAt;
+  }
+
+  async save() {
+    if (this._id) {
+      await this.api.update(this._id, this.serialize());
+    } else {
+      const { _id, createdAt } = await this.api.create(this.serialize());
+      this._id = _id;
+      this.createdAt = createdAt;
+    }
+  }
+
+  async delete() {
+    await this.api.delete(this._id);
+  }
 
   toObject() {
+    return {};
+  }
+
+  serialize() {
     return {};
   }
 }

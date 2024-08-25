78c228ec8a10c0c5548d884b8e69218e98981c9e
roshiroku <shir.oraan@gmail.com>
2024-08-25 20:50:55 +0300

    add pagination and caching to card page

--- src/pages/cards/CardsPage.jsx
+++ src/pages/cards/CardsPage.jsx
@@ -1,23 +1,40 @@
-import { useEffect, useState } from "react";
-import CardsAPI from "../../services/CardsAPI";
-import { Box, Grid } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+import { Grid, Pagination } from "@mui/material";
 import CardModel from "../../models/CardModel";
 import Card from "../../components/cards/Card";
 
 export default function CardsPage() {
-  const [cards, setCards] = useState();
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [perPage] = useState(24);
+  const [cards, setCards] = useState(CardModel.loadFromStorage());
+  const pageCount = useMemo(() => Math.ceil(cards.length / perPage), [cards, perPage]);
+  const start = useMemo(() => (page - 1) * perPage, [page, perPage]);
+  const end = useMemo(() => start + perPage, [start]);
 
   useEffect(() => {
-    CardsAPI.getAll().then(cards => setCards(cards.map(data => new CardModel(data))));
+    CardModel.loadMany().then(cards => {
+      setLoading(false);
+      setCards(cards);
+      CardModel.saveToStorage(cards.slice(0, 24));
+    });
   }, []);
 
   return (
-    <Grid container spacing={2} paddingY={2}>
-      {cards?.map(card => (
-        <Grid key={card._id} item xs={12} md={3}>
-          <Card {...card} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container spacing={2} paddingY={2}>
+        {cards.slice(start, end).map(card => (
+          <Grid key={card._id} item xs={12} md={3}>
+            <Card {...card} />
+          </Grid>
+        ))}
+      </Grid>
+      <Pagination
+        count={pageCount}
+        page={page}
+        onChange={(_, value) => setPage(value)}
+        shape="rounded"
+      />
+    </>
   );
 }

9c791b770364388488b257ef0bdcc17e6561708c
roshiroku <shir.oraan@gmail.com>
2024-08-25 20:50:40 +0300

    add model load many and caching

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,9 +1,30 @@
 export default class Model {
   static api;
 
+  static async loadMany(...ids) {
+    let models = [];
+
+    if (ids.length) {
+      models = await Promise.all(ids.map(id => this.api.read(id)));
+    } else {
+      models = await this.api.getAll();
+    }
+
+    return models.map(data => new this(data));
+  }
+
   static async load(id) {
     const data = await this.api.read(id);
     return new this(data);
+  }
+
+  static loadFromStorage(key = `${this.name.toLowerCase()}s`) {
+    const models = JSON.parse(localStorage.getItem(key)) || [];
+    return models.map(data => new this(data));
+  }
+
+  static saveToStorage(models, key = `${this.name.toLowerCase()}s`) {
+    localStorage.setItem(key, JSON.stringify(models));
   }
 
   static fromObject({ }) {

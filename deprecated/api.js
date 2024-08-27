efefde94d7ede27ca7017df3a153c53d8dc785d0
roshiroku <shir.oraan@gmail.com>
2024-08-27 14:09:59 +0300

    add deprecated files

---
+++ deprecated/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default async function api(url, { data = undefined, method = "get" } = {}) {
+  try {
+    const res = await axios[method](url, data);
+    return res.data;
+  } catch (e) {
+    throw new Error(e.response?.data || e.message);
+  }
+}

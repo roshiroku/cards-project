b1dcf3717c7abfd85243d04ba995068aa74a6057
roshiroku <shir.oraan@gmail.com>
2024-08-30 20:40:06 +0300

    move theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Router from "./Router";
 import AuthenticationProvider from "./providers/AuthenticationProvider";
+import ThemeProvider from "./providers/ThemeProvider";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthenticationProvider>
-        <Layout>
-          <Router />
-        </Layout>
+        <ThemeProvider>
+          <Layout>
+            <Router />
+          </Layout>
+        </ThemeProvider>
       </AuthenticationProvider>
     </BrowserRouter>
   );

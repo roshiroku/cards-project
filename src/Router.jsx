5a2e5ecd6f356a31f9a2e6852ef704b17a5191c0
roshiroku <shir.oraan@gmail.com>
2024-08-30 20:42:05 +0300

    add about page route

--- src/Router.jsx
+++ src/Router.jsx
@@ -31,6 +31,9 @@ export default function Router() {
   return (
     <Routes>
       <Route path={ROUTES.root} element={<CardsPage />} />
+
+      <Route path={ROUTES.about} element={<AboutPage />} />
+
       <Route path={ROUTES.myCards} element={<MyCardsPage />} />
       <Route path={ROUTES.favCards} element={<FavoriteCardsPage />} />
       <Route path={`${ROUTES.cardInfo}/:id`} element={<CardInfoPage />} />

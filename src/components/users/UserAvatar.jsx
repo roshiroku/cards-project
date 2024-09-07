e2cc4cda8857ee04653f7745ed9b82a2ed7fb616
roshiroku <shir.oraan@gmail.com>
2024-09-07 23:25:52 +0300

    fix user avatar

--- src/components/users/UserAvatar.jsx
+++ src/components/users/UserAvatar.jsx
@@ -2,12 +2,16 @@ import { Avatar } from "@mui/material";
 import { capitalize, stringToColor } from "../../utils/string";
 import { useMemo } from "react";
 
+const DEFAULT_AVATAR = "https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png";
+
 export default function UserAvatar({ user }) {
+  const src = useMemo(() => user.image.url == DEFAULT_AVATAR ? "" : user.image.url, [user.image]);
+
   const alt = useMemo(() => {
     return user.image.alt || capitalize(`${user.name.first} ${user.name.last}`);
   }, [user.image, user.name]);
 
   const bgcolor = useMemo(() => stringToColor(user.email), [user.email]);
 
-  return <Avatar src={user.image.src} alt={alt} sx={{ bgcolor }} />;
+  return <Avatar {...{ src, alt }} sx={{ bgcolor }} />;
 }

96619f3e5cad1c03314c4363ede7d8801ae9e997
roshiroku <shir.oraan@gmail.com>
2024-09-07 16:23:05 +0300

    update card actions

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,5 +1,5 @@
 import { Call, Delete, Edit, Favorite } from "@mui/icons-material";
-import { CardActionArea, CardMedia, CardHeader as MUICardHeader, Divider, CardContent, Typography, Card as MUICard, CardActions as MUICardActions, Box, IconButton } from "@mui/material";
+import { CardActionArea, CardMedia, CardHeader as MUICardHeader, Divider, CardContent, Typography, Card as MUICard, CardActions as MUICardActions, Box, IconButton, CircularProgress } from "@mui/material";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../Router";
 import { useCallback, useLayoutEffect, useMemo, useState } from "react";
@@ -71,24 +71,24 @@ export function CardActions({ id, ownerId, phone, likes, onChange }) {
   const { user } = useAuthentication();
   const { setNotification } = usePageUI();
   const [isFav, setIsFav] = useState(likes.includes(user?._id));
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const handleDelete = useErrorCallback(async () => {
     if (confirm("Are you sure you want to remove card?")) {
+      setIsDeleting(true);
       const card = await CardModel.load(id);
-      const deletePromise = card.delete();
-      user.cards = user.cards?.filter(({ _id }) => _id != card._id);
+      await card.delete().finally(() => setIsDeleting(false));
       onChange && onChange();
-      await deletePromise;
       setNotification({ message: "Card deleted", severity: "success" });
     }
   }, [onChange]);
 
   const toggleFav = useErrorCallback(async () => {
     const card = await CardModel.load(id);
+    const update = () => setIsFav(card.isLikedBy(user)) || onChange && onChange();
     const likePromise = card.toggleLike(user);
-    setIsFav(card.isLikedBy(user));
-    onChange && onChange();
-    await likePromise;
+    update();
+    await likePromise.finally(update);
   }, [id, user]);
 
   useLayoutEffect(() => {
@@ -99,12 +99,12 @@ export function CardActions({ id, ownerId, phone, likes, onChange }) {
     <MUICardActions sx={{ justifyContent: "space-between" }}>
       <Box display="flex">
         {
-          user?._id == ownerId &&
+          (user?._id == ownerId || user?.isAdmin) &&
           <>
-            <IconButton onClick={handleDelete}>
-              <Delete />
+            <IconButton onClick={handleDelete} disabled={isDeleting}>
+              {isDeleting ? <CircularProgress size={20} /> : <Delete />}
             </IconButton>
-            <IconButton LinkComponent={Link} to={`${ROUTES.editCard}/${id}`}>
+            <IconButton LinkComponent={Link} to={`${ROUTES.editCard}/${id}`} disabled={isDeleting}>
               <Edit />
             </IconButton>
           </>
@@ -115,12 +115,13 @@ export function CardActions({ id, ownerId, phone, likes, onChange }) {
           LinkComponent={Link}
           to={`https://api.whatsapp.com/send/?phone=${phone}&text&type=phone_number&app_absent=0`}
           target="_blank"
+          disabled={isDeleting}
         >
           <Call />
         </IconButton>
         {
-          user &&
-          <IconButton onClick={toggleFav} >
+          // user &&
+          <IconButton onClick={toggleFav} disabled={isDeleting}>
             <Favorite sx={{ color: isFav ? "red" : "gray" }} />
           </IconButton>
         }

84448aa96c20bb631682198880e7f7513cd62236
roshiroku <shir.oraan@gmail.com>
2024-09-07 03:16:21 +0300

    fix avatar memoization

--- src/components/users/UserAvatar.jsx
+++ src/components/users/UserAvatar.jsx
@@ -1,8 +1,8 @@
 import { Avatar } from "@mui/material";
 import { capitalize, stringToColor } from "../../utils/string";
-import { memo, useMemo } from "react";
+import { useMemo } from "react";
 
-export default memo(function UserAvatar({ user }) {
+export default function UserAvatar({ user }) {
   const alt = useMemo(() => {
     return user.image.alt || capitalize(`${user.name.first} ${user.name.last}`);
   }, [user.image, user.name]);
@@ -10,4 +10,4 @@ export default memo(function UserAvatar({ user }) {
   const bgcolor = useMemo(() => stringToColor(user.email), [user.email]);
 
   return <Avatar src={user.image.src} alt={alt} sx={{ bgcolor }} />;
-});
+}

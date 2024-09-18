9555079aeeca110c2bf417efa9f21cc4b2897de3
roshiroku <shir.oraan@gmail.com>
2024-09-18 23:56:19 +0300

    refactor layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,42 +1,22 @@
 import { Alert, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Snackbar } from "@mui/material";
 import Footer from "./Footer";
 import Header from "./Header";
-import { useTheme } from "../../providers/ThemeProvider";
 import SearchProvider from "../../providers/SearchProvider";
 import { useCallback, useEffect, useState } from "react";
 import { usePageUI } from "../../providers/PageUIProvider";
 import "../../style/layout.scss";
 
 export default function Layout({ children }) {
-  const [showPopup, setShowPopup] = useState(false);
-  const [showNotification, setShowNotification] = useState(false);
-
-  const { theme } = useTheme();
-  const { popup, notification } = usePageUI();
-
-  const closePopup = useCallback((confirm = false) => {
-    popup?.resolve && popup.resolve(confirm);
-    setShowPopup(false);
-  }, [popup]);
-
-  const closeNotification = useCallback(() => setShowNotification(false), []);
-
-  useEffect(() => {
-    setShowPopup(!!popup);
-  }, [popup]);
-
-  useEffect(() => {
-    setShowNotification(!!notification);
-  }, [notification]);
-
   return (
     <SearchProvider>
       <Box
-        display="flex"
-        flexDirection="column"
-        flexGrow="1"
-        color={theme.palette.text.primary}
-        bgcolor={theme.palette.background.default}
+        sx={theme => ({
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: "1",
+          color: theme.palette.text.primary,
+          bgcolor: theme.palette.background.default
+        })}
       >
         <Header />
         <Box component="main" sx={{ flexGrow: 1 }}>
@@ -44,43 +24,76 @@ export default function Layout({ children }) {
         </Box>
         <Footer />
       </Box>
-      <Snackbar
-        key={notification?.message}
-        open={showNotification}
-        autoHideDuration={3000}
-        onClose={closeNotification}
-      >
-        <Alert
-          onClose={closeNotification}
-          severity={notification?.severity}
-          variant="filled"
-          sx={{ width: "100%" }}
-        >
-          {notification?.message}
-        </Alert>
-      </Snackbar>
-      <Dialog open={showPopup} onClose={() => closePopup(false)}>
-        {popup?.title && <DialogTitle children={popup.title} />}
-        {popup?.text && (
-          <DialogContent>
-            <DialogContentText children={popup.text} />
-          </DialogContent>
-        )}
-        {popup?.actions && (
-          <DialogActions>
-            {popup.actions == true ? (
-              <>
-                <Button onClick={() => closePopup(false)}>Cancel</Button>
-                <Button onClick={() => closePopup(true)}>
-                  Confirm
-                </Button>
-              </>
-            ) : (
-              popup.actions
-            )}
-          </DialogActions>
-        )}
-      </Dialog>
+      <Notification />
+      <Popup />
     </SearchProvider>
+  );
+}
+
+export function Notification() {
+  const { notification } = usePageUI();
+  const [open, setOpen] = useState(false);
+
+  const onClose = useCallback(() => setOpen(false), []);
+
+  useEffect(() => {
+    setOpen(!!notification);
+  }, [notification]);
+
+  return (
+    <Snackbar
+      key={notification?.message}
+      open={open}
+      autoHideDuration={3000}
+      onClose={onClose}
+    >
+      <Alert
+        onClose={onClose}
+        severity={notification?.severity}
+        variant="filled"
+        sx={{ width: "100%" }}
+      >
+        {notification?.message}
+      </Alert>
+    </Snackbar>
+  );
+}
+
+export function Popup() {
+  const { popup } = usePageUI();
+  const [open, setOpen] = useState(false);
+
+  const onClose = useCallback((confirm = false) => {
+    popup?.resolve && popup.resolve(confirm);
+    setOpen(false);
+  }, [popup]);
+
+  useEffect(() => {
+    setOpen(!!popup);
+  }, [popup]);
+
+  return (
+    <Dialog open={open} onClose={() => onClose(false)}>
+      {popup?.title && <DialogTitle>{popup.title}</DialogTitle>}
+      {popup?.text && (
+        <DialogContent>
+          <DialogContentText>{popup.text}</DialogContentText>
+        </DialogContent>
+      )}
+      {popup?.actions && (
+        <DialogActions>
+          {popup.actions == true ? (
+            <>
+              <Button onClick={() => onClose(false)}>Cancel</Button>
+              <Button onClick={() => onClose(true)} color="primary">
+                Confirm
+              </Button>
+            </>
+          ) : (
+            popup.actions
+          )}
+        </DialogActions>
+      )}
+    </Dialog>
   );
 }
